af4f348653373e9f5c68589eda7082ac571cf747
100Daisy <100Daisy@protonmail.com>
2021-05-05 10:14:07 +0200

    ci: clone kernel
    Stock branch for now

--- twrp-build.sh
+++ twrp-build.sh
@@ -14,6 +14,7 @@ repo sync  -f --force-sync --no-clone-bundle --no-tags -j$(nproc --all)
 
 # Clone device tree and common tree
 git clone --depth=1 https://github.com/100Daisy/android_device_motorola_guamp -b android-10 device/motorola/guamp
+git clone https://github.com/100Daisy/android_kernel_motorola_guamp -b STOCK kernel/motorola/guamp
 
 # Build recovery image
 export ALLOW_MISSING_DEPENDENCIES=true; . build/envsetup.sh; lunch omni_guamp-eng; make -j$(nproc --all) recoveryimage
