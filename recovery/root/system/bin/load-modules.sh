24bcf6db541f9aa19651c969ea07ed1337d243c7
100Daisy <100Daisy@protonmail.com>
2021-05-09 11:13:23 +0200

    cebu: Run novatek and focaltech drivers

--- recovery/root/system/bin/load-modules.sh
+++ recovery/root/system/bin/load-modules.sh
@@ -1,11 +1,7 @@
 #!/system/bin/sh
 
-insmod /vendor/lib/modules/chipone_tddi_mmi.ko
-insmod /vendor/lib/modules/fpc1020_mmi.ko
-insmod /vendor/lib/modules/goodix_fod_mmi.ko
-insmod /vendor/lib/modules/himax_v3_mmi.ko
-insmod /vendor/lib/modules/himax_v3_mmi_hx83102d.ko
-insmod /vendor/lib/modules/ili9882_mmi.ko
+insmod /vendor/lib/modules/nova_0flash_mmi.ko
+insmod /vendor/lib/modules/focaltech_0flash_mmi.ko
 insmod /vendor/lib/modules/mmi_annotate.ko
 insmod /vendor/lib/modules/mmi_info.ko
 insmod /vendor/lib/modules/mmi_sys_temp.ko
