61960d617fc06246b45c43e95113434036004b95
100Daisy <48625180+100Daisy@users.noreply.github.com>
2021-05-24 10:53:16 +0200

    Update load-modules.sh

--- recovery/root/system/bin/load-modules.sh
+++ recovery/root/system/bin/load-modules.sh
@@ -6,11 +6,7 @@ insmod /vendor/lib/modules/goodix_fod_mmi.ko
 insmod /vendor/lib/modules/himax_v3_mmi.ko
 insmod /vendor/lib/modules/himax_v3_mmi_hx83102d.ko
 insmod /vendor/lib/modules/ili9882_mmi.ko
-insmod /vendor/lib/modules/mmi_annotate.ko
-insmod /vendor/lib/modules/mmi_info.ko
-insmod /vendor/lib/modules/mmi_sys_temp.ko
-insmod /vendor/lib/modules/sensors_class.ko
-insmod /vendor/lib/modules/exfat.ko
+
 wait 1
 
 setprop modules.loaded 1
