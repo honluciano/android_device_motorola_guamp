582ae9fcd4495567119c6a678eb0ca6283986a75
honluciano <81577980+honluciano@users.noreply.github.com>
2021-05-25 13:54:28 -0300

    Update twrp-build.sh

--- twrp-build.sh
+++ twrp-build.sh
@@ -15,7 +15,7 @@ repo sync  -f --force-sync --no-clone-bundle --no-tags -j$(nproc --all)
 
 # Clone device tree and common tree
 git clone --depth=1 https://github.com/100Daisy/android_device_motorola_guamp -b android-10 device/motorola/guamp
-
+git clone --depth=1 https://github.com/100Daisy/android_kernel_motorola_guamp.git -b twrp-10 kernel/motorola/guamp
 # Build recovery image
 export ALLOW_MISSING_DEPENDENCIES=true; . build/envsetup.sh; lunch omni_guamp-eng; make -j$(nproc --all) recoveryimage
 # Rename and copy the files
